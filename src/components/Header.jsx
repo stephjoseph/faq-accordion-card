b01aec58c9570b0bdccfe5a3583384578c0130a9
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-26 13:55:30 +0800

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1 className="absolute left-[-999999px] top-[-999999px] text-white">
+        FAQ Accordion Card
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
